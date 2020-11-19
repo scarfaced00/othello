82aa90d6e8ba211494bbce5f23e89b11a5aa52f5
scarfaced00 <jazmyne@naver.com>
2020-11-19 13:49:23 +0900

    empty board making

--- board.c
+++ board.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
-
-
+#define N 6
+int gameboard[N][N]; 
 int main() //빈 보드 출력하기 
 {
-	char color='O';
-	int col,row;
+	int col,row;//열과 행 
 	printf("  0 1 2 3 4 5\n -------------\n");
 	//가로줄 하나씩
 	for(row=0;row<6;row++)
 		{
 			printf("%d|",row);//행번호 출력
 			for(col=0;col<6;col++)
-				printf("%c|",color);//검은색 이면 x, 흰색이면 O,아무것도 없으면 공백출력 
+			{
+				if(gameboard[row][col]==0)
+					printf(" |");//gameboard원소가 0이면 공백이 출력되게함
+				else
+					printf("%d|",gameboard[row][col]);
+			}
+			//검은색 이면 x, 흰색이면 O,아무것도 없으면 공백출력 
 			printf("\n -------------\n"); //행바꾸기 선 
 		 }
 } 
