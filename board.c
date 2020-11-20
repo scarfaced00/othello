14ccfef2d21ba2938322a40c1f944e03e11305d9
scarfaced00 <jazmyne@naver.com>
2020-11-21 08:32:53 +0900

    initial board

--- board.c
+++ board.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #define N 6
+
 int gameboard[N][N]; 
 int main() //빈 보드 출력하기 
 /*빈 칸=0, 흰 알='O', 검은색 알='X'*/ 
 {
+	int i,j;
+	gameboard[2][2]='O';
+	gameboard[2][3]='X';
+	gameboard[3][2]='X';
+	gameboard[3][3]='O';
 	int col,row;//열과 행 
 	printf("  0 1 2 3 4 5\n -------------\n");
 	//가로줄 하나씩
@@ -15,7 +21,7 @@ int main() //빈 보드 출력하기
 				if(gameboard[row][col]==0)
 					printf(" |");//gameboard원소가 0이면 공백이 출력되게함
 				else
-					printf("%d|",gameboard[row][col]);
+					printf("%c|",gameboard[row][col]);
 			}
 			//검은색 이면 x, 흰색이면 O,아무것도 없으면 공백출력 
 			printf("\n -------------\n"); //행바꾸기 선 
