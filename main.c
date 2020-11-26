e54ff7997b84e72ac952cce442dba147e486032e
scarfaced00 <jazmyne@naver.com>
2020-11-26 21:21:09 +0900

    Update main.c

--- main.c
+++ main.c
@@ -84,7 +84,7 @@ void input_index(int *row, int *col, int player) //좌표 입력받기
  {
  	printf("\nput a new %s othello : ",players[player-1]);
  	scanf("%d %d",row,col);
- 	while(!is_room_exist(row,col))//존재하지않는칸이면 계속 입력받음 
+ 	while(!room_exist(row,col))//존재하지않는칸이면 계속 입력받음 
  	{
  		printf("Non existent coordinate\n");
  		printf("\nput a new %s othello : ",players[player-1]);
