fd38149c841de330ed9c0817c2276efebf6c26f8
scarfaced00 <jazmyne@naver.com>
2020-11-26 14:13:33 +0900

    Update main.c

--- main.c
+++ main.c
@@ -35,7 +35,7 @@ int ok()
                 return 1;
     }
 }
-int player_change(int player)
+int player_change(int player)//차례바꾸기 
 {
 	return (playere%2 +1);
 }
@@ -97,3 +97,31 @@ void count_colors()
 	}
 	printf("\nSTATUS - WHITE : %d, BLACK : %d\n",white,black);
 }
+void flip(int row, int col, int player)
+{
+	int i,j;
+	char user, opp;
+	if(player==1)
+	{
+		user=BLACK;
+		opp=WHITE;
+	}
+	else
+	{
+		user=WHITE;
+		opp=BLACK;
+	}
+	//입력 칸의 왼쪽 가로 해당되는 알 뒤집기 
+	i=col-1;
+	while(gameboard[row][i]==user) //왼쪽 가로에 사용자 알 있으면 계속 왼쪽으로 
+		i--;
+	if(gamebaord[row][i]==opp)//왼쪽 가로 점검하다 상대방 알 만나면 
+	{
+		i=col-1; 
+		while(gameboard[row][i]==opp)//왼쪽가로에 있는 상대방말을 모두 사용자의 말로 변경 
+		{
+			gameboard[row][i]=user;
+			i--;
+		}
+	}
+ } 
