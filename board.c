a92eec656b1a3d8f08a73f7d0d0e65e025894705
scarfaced00 <jazmyne@naver.com>
2020-11-26 23:47:04 +0900

    1

--- board.c
+++ board.c
@@ -1,6 +1,12 @@
+#include <stdio.h>
 #define N 6
+#define EMPTY ' '
+#define WHITE 79 //'O'
+#define BLACK 88 //'X'
+extern gameboard[N][N];
+extern int col,row;
 void init_othello() //보드 초기화 
-/*빈 칸=' ', 흰 알='O', 검은색 알='X'*/ 
+//빈 칸=' ', 흰 알='O', 검은색 알='X'
 {
 	//모든 칸 빈칸으로 
 	int i,j;
@@ -13,8 +19,8 @@ void init_othello() //보드 초기화
 	gameboard[N/2][N/2]=WHITE;
 	gameboard[N/2][N/2-1]=BLACK;
 }
- 
-extern void print_othello()//배치상태출력 
+
+void print_othello()//배치상태출력 
 {
 	printf("  0 1 2 3 4 5\n -------------\n");
 	//가로줄 하나씩
@@ -31,28 +37,4 @@ extern void print_othello()//배치상태출력
 			//검은색 이면 x, 흰색이면 O,아무것도 없으면 공백출력 
 			printf("\n -------------\n"); //행바꾸기 선 
 		 }
-}
-
-int room_exist(int row, int col)//사용자가 입력한 값이 존재하는 칸인지 
-{
-	if(row>=0 && row<N && col>=0 && col<N)
-		return 1;
-}
-
-int flip_ok(int row, int col, int player) //뒤집을 수 있는 입력인지 
-{
-	int i, j
-	int ok;
-	char user, opp; 
-	//사용자와 상대편 설정 
-	if(player==1)
-	{
-		user=BLACK;
-		opp=WHITE; 
-	 } 
-	else{
-		user=WHITE;
-		opp=BLACK;
-	}
-	
 }
--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #define EMPTY ' '
 #define WHITE 79 //'O'
 #define BLACK 88 //'X'
+#include "head.h"
 int gameboard[N][N];
 int col,row;
 char players[2][10]={"black","white"};
@@ -11,40 +12,6 @@ int room_exist(int row, int col)//사용자가 입력한 값이 존재하는 칸인지
 {
 	if(row>=0 && row<N && col>=0 && col<N)
 		return 1;
-}
-
-void init_othello() //보드 초기화 
-//빈 칸=' ', 흰 알='O', 검은색 알='X'
-{
-	//모든 칸 빈칸으로 
-	int i,j;
-	for(i=0;i<N;i++)
-		for(j=0;j<N;j++)
-			gameboard[i][j]=EMPTY; 
-	//처음 4개알 두기 
-	gameboard[N/2-1][N/2]=BLACK;
-	gameboard[N/2-1][N/2-1]=WHITE;
-	gameboard[N/2][N/2]=WHITE;
-	gameboard[N/2][N/2-1]=BLACK;
-}
-
-void print_othello()//배치상태출력 
-{
-	printf("  0 1 2 3 4 5\n -------------\n");
-	//가로줄 하나씩
-	for(row=0;row<N;row++)
-		{
-			printf("%d|",row);//행번호 출력
-			for(col=0;col<N;col++)
-			{
-				if(gameboard[row][col]==' ')
-					printf(" |");//gameboard원소가 0이면 공백이 출력되게함
-				else
-					printf("%c|",gameboard[row][col]);
-			}
-			//검은색 이면 x, 흰색이면 O,아무것도 없으면 공백출력 
-			printf("\n -------------\n"); //행바꾸기 선 
-		 }
 }
 
 int player_change(int player)//차례바꾸기 
